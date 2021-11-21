9dd042a13d04b00c5a3aa9b65f24c36dd7f7d447
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2021-11-21 19:35:27 +0200

    Code First Model

---
+++ EntityRelations/1.StudentSystem/1.Student System/Migrations/20211121162437_InitialV2.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Migrations
+{
+    public partial class InitialV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Homeworks_Courses_CourseId",
+                table: "Homeworks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Homeworks_Students_StudentId",
+                table: "Homeworks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Homeworks",
+                table: "Homeworks");
+
+            migrationBuilder.RenameTable(
+                name: "Homeworks",
+                newName: "HomeworksSubmissions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Homeworks_StudentId",
+                table: "HomeworksSubmissions",
+                newName: "IX_HomeworksSubmissions_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Homeworks_CourseId",
+                table: "HomeworksSubmissions",
+                newName: "IX_HomeworksSubmissions_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HomeworksSubmissions",
+                table: "HomeworksSubmissions",
+                column: "HomeworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworksSubmissions_Courses_CourseId",
+                table: "HomeworksSubmissions",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworksSubmissions_Students_StudentId",
+                table: "HomeworksSubmissions",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworksSubmissions_Courses_CourseId",
+                table: "HomeworksSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworksSubmissions_Students_StudentId",
+                table: "HomeworksSubmissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HomeworksSubmissions",
+                table: "HomeworksSubmissions");
+
+            migrationBuilder.RenameTable(
+                name: "HomeworksSubmissions",
+                newName: "Homeworks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworksSubmissions_StudentId",
+                table: "Homeworks",
+                newName: "IX_Homeworks_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworksSubmissions_CourseId",
+                table: "Homeworks",
+                newName: "IX_Homeworks_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Homeworks",
+                table: "Homeworks",
+                column: "HomeworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Homeworks_Courses_CourseId",
+                table: "Homeworks",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Homeworks_Students_StudentId",
+                table: "Homeworks",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ EntityRelations/1.StudentSystem/1.Student System/Migrations/20211121163543_InitialV3.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Migrations
+{
+    public partial class InitialV3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworksSubmissions_Courses_CourseId",
+                table: "HomeworksSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworksSubmissions_Students_StudentId",
+                table: "HomeworksSubmissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HomeworksSubmissions",
+                table: "HomeworksSubmissions");
+
+            migrationBuilder.RenameTable(
+                name: "HomeworksSubmissions",
+                newName: "HomeworkSubmissions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworksSubmissions_StudentId",
+                table: "HomeworkSubmissions",
+                newName: "IX_HomeworkSubmissions_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworksSubmissions_CourseId",
+                table: "HomeworkSubmissions",
+                newName: "IX_HomeworkSubmissions_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HomeworkSubmissions",
+                table: "HomeworkSubmissions",
+                column: "HomeworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Courses_CourseId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeworkSubmissions_Students_StudentId",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_HomeworkSubmissions",
+                table: "HomeworkSubmissions");
+
+            migrationBuilder.RenameTable(
+                name: "HomeworkSubmissions",
+                newName: "HomeworksSubmissions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworkSubmissions_StudentId",
+                table: "HomeworksSubmissions",
+                newName: "IX_HomeworksSubmissions_StudentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_HomeworkSubmissions_CourseId",
+                table: "HomeworksSubmissions",
+                newName: "IX_HomeworksSubmissions_CourseId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_HomeworksSubmissions",
+                table: "HomeworksSubmissions",
+                column: "HomeworkId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworksSubmissions_Courses_CourseId",
+                table: "HomeworksSubmissions",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeworksSubmissions_Students_StudentId",
+                table: "HomeworksSubmissions",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
